4d5d9d4e2e3690a33799f1e66fa069553ba401a8
Muhammad Ahsan Abrar <iahsanabrar@gmail.com>
2021-03-15 14:29:02 +0500

    shift to laravel 8

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,30 +1,31 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<title>Entrepreneurs Motivational Quotes</title>
+        <title>Entrepreneurs Motivational Quotes</title>
 
-		<!-- CSRF Token -->
-		<meta name="csrf-token" content="{{ csrf_token() }}">
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-		<!-- Fonts -->
-		<link href="https://fonts.googleapis.com/css?family=Raleway:100" rel="stylesheet" type="text/css">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100" rel="stylesheet" type="text/css">
 
-		<!-- Styles -->
-		<link rel="stylesheet" type="text/css" href="/css/app.css">
-	</head>
-	<body>
-		<div id="app" v-cloak>
-			<transition name="scale">
-				<div class="quote" v-if="show" v-text="quote"></div>
-			</transition>
-		</div>
+        <!-- Styles -->
+        <link rel="stylesheet" type="text/css" href="/css/app.css">
+    </head>
+    <body>
+        <div id="app" v-cloak>
+            <transition name="scale">
+                <div class="quote" v-if="show" v-text="quote"></div>
+            </transition>
+        </div>
 
 
-		<!-- Scripts -->
-		<script src="/js/app.js"></script>
-	</body>
+        <!-- Scripts -->
+        <script src="/js/app.js"></script>
+    </body>
 </html>
+
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
-let mix = require('laravel-mix')
+const mix = require('laravel-mix')
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.sass', 'public/css')
+mix.js('resources/js/app.js', 'public/js')
+   .sass('resources/sass/app.sass', 'public/css')
    .disableNotifications()
